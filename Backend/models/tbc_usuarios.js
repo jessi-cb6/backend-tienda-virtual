1a4a9a124a377be514c82c9f6c0006a132c84db5
jessi-cb6 <240491@utxicotepec.edu.mx>
2026-03-13 09:53:12 -0600

    Terminar

---
+++ Backend/models/tbc_usuarios.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tbc_usuarios extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tbc_usuarios.init({
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    direccion:{
+type :DataTypes.STRING(100),
+allowNull:false
+    } ,
+    password:{
+      type:DataTypes.STRING(255),
+      allowNull:false
+    } ,
+        email:{
+        type: DataTypes.STRING(120),
+        allowNull:false
+
+        },
+
+    telefono: {
+      type:DataTypes.STRING(15),
+      allowNull:false
+    },
+    rol: {
+      type: DataTypes.ENUM('admin', 'cliente'),
+      allowNull: false,
+      defaultValue: 'cliente'
+    },
+    fecha_registro: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'tbc_usuarios',
+  });
+  return tbc_usuarios;
+};
